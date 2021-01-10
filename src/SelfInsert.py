ef084acc61dee3360a1241926497340dddc1d0a3
semitrivial <alexander@math.ohio-state.edu>
2021-01-09 23:44:57 -0500

    Revert "Make SelfInsert.py more 'thread safe'"
    This reverts commit 0702e8497ed3b9cbfa3745eb252088c2ddddbf61.

--- src/SelfInsert.py
+++ src/SelfInsert.py
@@ -1,26 +1,4 @@
 def self_insert(base_env):
-    hat_dict = {}
-    reverse_hat_dict = {}
-
-    def reverse_hat(x, y):
-        if not((x,y) in reverse_hat_dict):
-            counter = len(reverse_hat_dict)
-            reverse_hat_dict[(x,y)] = counter
-            hat_dict[counter] = [x,y]
-
-        return reverse_hat_dict[(x,y)]
-
-    def hat(n):
-        return hat_dict[n]
-
-    def decode_observations(prompt):
-        if len(prompt) < 3:
-            _, obs = prompt
-            return hat(obs)
-        else:
-            _, obs, action = prompt[0:3]
-            return hat(obs) + [action] + decode_observations(prompt[3:])
-
     def modified_env(T, play):
         if len(play) == 0:
             return base_env(T, play)
@@ -33,3 +11,25 @@ def self_insert(base_env):
         return [reward, obs]
 
     return modified_env
+
+def decode_observations(prompt):
+    if len(prompt) < 3:
+        _, obs = prompt
+        return hat(obs)
+    else:
+        _, obs, action = prompt[0:3]
+        return hat(obs) + [action] + decode_observations(prompt[3:])
+
+hat_dict = {}
+reverse_hat_dict = {}
+
+def reverse_hat(x, y):
+    if not((x,y) in reverse_hat_dict):
+        counter = len(reverse_hat_dict)
+        reverse_hat_dict[(x,y)] = counter
+        hat_dict[counter] = [x,y]
+
+    return reverse_hat_dict[(x,y)]
+
+def hat(n):
+    return hat_dict[n]
