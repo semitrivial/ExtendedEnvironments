983713f9d34c1e97a35f386de9a58b864f977ce8
semitrivial <alexander@math.ohio-state.edu>
2021-03-11 22:40:42 -0500

    Add test_nutrition

--- src/test/ad_hoc_tests.py
+++ src/test/ad_hoc_tests.py
@@ -1,6 +1,8 @@
 def run_ad_hoc_tests():
     print("Testing reverse_prompt from BackwardConsciousness.py")
     test_reverse_prompt()
+    print("Testing nutrition from CryingBaby.py")
+    test_nutrition()
 
 def test_reverse_prompt():
     from BackwardConsciousness import reverse_prompt
@@ -17,3 +19,23 @@ def test_reverse_prompt():
     prompt = reverse_prompt(play)
     assert prompt == ["r2", "o2", "a1", "r1", "o1", "a0", "r0", "o0"]
 
+def test_nutrition():
+    from CryingBaby import nutrition, FEED, DONTFEED
+
+    play = []
+    assert nutrition(play) == 100
+
+    play = ["r", "o", FEED]
+    assert nutrition(play) == (100 - 1) + 25
+
+    play = ["r", "o", DONTFEED]
+    assert nutrition(play) == 100 - 1
+
+    play = ["r", "o", DONTFEED] * 100
+    assert nutrition(play) == 100 - 100
+
+    play = ["r", "o", DONTFEED, "r", "o", FEED]
+    assert nutrition(play) == (100-2) + 25
+
+    play = ["r", "o", FEED] * 100
+    assert nutrition(play) == (100-100) + 100*25
