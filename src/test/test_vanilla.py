3d3e2e254eeb29a8693d594b8a7e5516495837a2
semitrivial <alexander@math.ohio-state.edu>
2021-03-15 23:58:38 -0400

    Add test_mazes

--- src/test/test_vanilla.py
+++ src/test/test_vanilla.py
@@ -1,3 +1,5 @@
+from random import random
+
 from util import run_environment
 
 def test_vanilla():
@@ -5,6 +7,8 @@ def test_vanilla():
     test_bandits()
     print("Testing vanilla/GuessTheNumber.py")
     test_guess_the_number()
+    print("Testing vanilla/Maze.py")
+    test_mazes()
 
 def test_bandits():
     from vanilla.Bandit import Bandit1, Bandit2, Bandit3, Bandit4, Bandit5
@@ -30,3 +34,45 @@ def test_guess_the_number():
     result = run_environment(GuessTheNumber, blank_observation_observer, 100)
     assert result['total_reward'] == len(blank_observations)-1
 
+def test_mazes():
+    from vanilla.Maze import Maze1, Maze2, Maze3, Maze4, Maze5
+
+    def learns_about_bad_moves(prompt):
+        bad_moves = {x:[] for x in range(10)}
+        for i in range(len(prompt)):
+            is_obs = (i%2)==1
+            if is_obs and i>1:
+                obs = prompt[i]
+                prev_obs = prompt[i-3]
+                prev_action = prompt[i-2]
+                prev_reward = prompt[i-1]
+                if (obs==prev_obs) or (obs==1 and prev_reward==0):
+                    if not(prev_action in bad_moves[prev_obs]):
+                        bad_moves[prev_obs] += [prev_action]
+
+        curr_room = prompt[-1]
+        while True:
+            door = int(random()*4)
+            if len(bad_moves[curr_room])==4:
+                return door
+            if not(door in bad_moves[curr_room]):
+                return door
+
+    for maze in [Maze2, Maze3, Maze4]:
+        result = run_environment(maze, learns_about_bad_moves, 50)
+        if result['total_reward'] == 0:
+            result = run_environment(maze, learns_about_bad_moves, 250)
+            assert result['total_reward']>0
+
+    for maze in [Maze1, Maze5]:
+        result = run_environment(maze, learns_about_bad_moves, 100)
+        if result['total_reward'] == 0:
+            result = run_environment(maze, learns_about_bad_moves, 500)
+            assert result['total_reward']>0
+
+    def always_goes_north(prompt):
+        return 0
+
+    for maze in [Maze1, Maze2, Maze3, Maze4, Maze5]:
+        result = run_environment(maze, always_goes_north, 50)
+        assert result['total_reward'] == 0
