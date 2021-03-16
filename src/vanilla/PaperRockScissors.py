fd031081da700a135014b79f460210801b9afc83
semitrivial <alexander@math.ohio-state.edu>
2021-03-15 22:51:10 -0400

    Initial draft of PaperRockScissors.py

---
+++ src/vanilla/PaperRockScissors.py
@@ -0,0 +1,22 @@
+from random import random
+
+PAPER, ROCK, SCISSORS = 0, 1, 2
+weaknesses = {PAPER: SCISSORS, ROCK: PAPER, SCISSORS: ROCK}
+
+def PaperRockScissors(T, play):
+    if len(play) == 0:
+        obs, reward = 0,0
+        return obs, reward
+
+    env_choice = int(random()*3)
+    agent_choice = play[-1]
+
+    if agent_choice == env_choice:
+        reward = 1
+    elif agent_choice == weaknesses[env_choice]:
+        reward = 2
+    else:
+        reward = 0
+
+    obs = env_choice
+    return reward, obs
