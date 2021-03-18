7653e1d8817d6c3bfd11094616f9a9ffabe5d227
semitrivial <alexander@math.ohio-state.edu>
2021-03-17 22:42:49 -0400

    Basic testing for RuntimeInspector

--- src/test/tests.py
+++ src/test/tests.py
@@ -8,6 +8,7 @@ from DejaVu import deja_vu
 from CryingBaby import crying_baby
 from FalseMemories import false_memories
 from BackwardConsciousness import backward_consciousness
+from RuntimeInspector import punish_slow_agent, punish_fast_agent
 
 def test_environment(env, env_name):
     for agent_name in agents.keys():
@@ -35,3 +36,9 @@ def test_false_memories():
 
 def test_backward_consciousness():
     test_environment(backward_consciousness, "backward_consciousness")
+
+def test_punish_slow_agent():
+    test_environment(punish_slow_agent, "punish_slow_agent")
+
+def test_punish_fast_agent():
+    test_environment(punish_fast_agent, "punish_fast_agent")
