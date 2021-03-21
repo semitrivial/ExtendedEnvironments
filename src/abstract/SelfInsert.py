ec4175f7c37ce3b6bea584ba7db4fffc71765b16
semitrivial <alexander@math.ohio-state.edu>
2021-03-21 18:33:14 -0400

    Initial draft of SelfInsert.py

---
+++ src/abstract/SelfInsert.py
@@ -0,0 +1,34 @@
+def SelfInsert(e):
+    def env(T, play):
+        if len(play) == 0:
+            reward, obs = e(T, play)
+            return reward, obs
+
+        prompt, action = play[:-1], play[-1]
+        modified_prompt = replace_rewards_with_encoded_rewards(prompt)
+        
+        reward = 1 if (action == T(modified_prompt)) else -1
+        obs = e(T, play)  # Encode a reward-obs pair as a single obs
+        return reward, obs
+
+    return env
+
+def replace_rewards_with_encoded_rewards(prompt):
+    num_obs = 1+(len(prompt)//3)
+    for i in range(num_obs):
+        obs = prompt[i*3+1]
+        try:
+            enc_reward, enc_obs = obs  # obs actually encodes a reward-obs pair
+        except TypeError:
+            # This would never be triggered naturally by an agent interacting
+            # with the environment in question, but is necessary to satisfy
+            # the definition of an environment (an environment must output
+            # reward-observation pairs when fed valid plays, even if those
+            # plays could never arise naturally by any agent interacting
+            # with said environment)
+            enc_reward, enc_obs = 0,0
+
+        prompt[i*3+0] = enc_reward  # replace true reward with encoded reward
+        prompt[i*3+1] = enc_obs  # strip reward component from observation
+
+    return prompt
