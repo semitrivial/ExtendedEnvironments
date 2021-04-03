7d03f2f873f6ea2971c679517832845fbed3bdd9
Samuel Alexander <sam@donotemail.com>
2021-04-03 13:33:55 -0400

    Commit temporary file gt_tmp.py

---
+++ src/gt_tmp.py
@@ -0,0 +1,11 @@
+from agents.SBL3_agents import agent_A2C, agent_DQN, agent_PPO
+from IgnoreRewards import ignore_rewards
+from GuardedTreasures import GuardedTreasures
+from util import run_environment
+from BackwardConsciousness import BackwardConsciousness
+from HistoryObservable import HistoryObservable
+
+e = HistoryObservable(GuardedTreasures)
+
+result = run_environment(e, agent_A2C, 10000)
+print(result['total_reward'])
