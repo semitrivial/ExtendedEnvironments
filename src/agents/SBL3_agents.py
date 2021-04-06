8247688352cddbf18ab91e7867efc2ed44233512
Samuel Alexander <sam@donotemail.com>
2021-04-06 16:27:56 -0400

    Split SBL agents into their own functions

--- src/agents/SBL3_agents.py
+++ src/agents/SBL3_agents.py
@@ -23,49 +23,76 @@ class DummyEnv(Env):
 
 dummy_env = DummyEnv()
 
-caches = {
-    SBL3.A2C: {},
-    SBL3.DQN: {},
-    SBL3.PPO: {},
-}
+cache_A2C = {}
+cache_DQN = {}
+cache_PPO = {}
 
-def SBL_agent(learner):
-    cache = caches[learner]
+def agent_A2C(prompt, num_legal_actions, num_possible_obs):
+    dummy_env.set_meta(num_legal_actions, num_possible_obs)
+    meta = (num_legal_actions, num_possible_obs)
+    num_observs = (len(prompt)+1)/3
+    train_on_len = 3*pow(2, int(log2(num_observs)))-1
+    train_on = prompt[:train_on_len]
 
-    def agent(prompt, num_legal_actions, num_possible_obs):
-        dummy_env.set_meta(num_legal_actions, num_possible_obs)
-        meta = (num_legal_actions, num_possible_obs)
-        num_observs = (len(prompt)+1)/3
-        train_on_len = 3*pow(2, int(log2(num_observs)))-1
-        train_on = prompt[:train_on_len]
+    if not((train_on, meta) in cache_A2C):
+        rewards = [train_on[i+0] for i in range(0,train_on_len,3)]
+        observs = [train_on[i+1] for i in range(0,train_on_len,3)]
+        dummy_env.set_rewards_and_observs(rewards, observs)
 
-        if not((train_on, meta) in cache):
-            rewards = [train_on[i+0] for i in range(0,train_on_len,3)]
-            observs = [train_on[i+1] for i in range(0,train_on_len,3)]
-            dummy_env.set_rewards_and_observs(rewards, observs)
+        A = SBL3.A2C('MlpPolicy', dummy_env, n_steps=len(rewards)-1, seed=0)
 
-            if learner == SBL3.PPO:
-                n_steps = len(rewards)-1
+        A.learn(len(rewards)-1)
+        cache_A2C[(train_on, meta)] = A
+    else:
+        A = cache_A2C[(train_on, meta)]
 
-                if n_steps < 2:
-                    return 0
+    action, _ = A.predict(prompt[-1])
+    return action
 
-                A = learner('MlpPolicy', dummy_env, n_steps=n_steps, batch_size=n_steps)
-            elif learner == SBL3.A2C:
-                A = learner('MlpPolicy', dummy_env, n_steps=len(rewards)-1)
-            else:
-                A = learner('MlpPolicy', dummy_env, train_freq=len(rewards)-1)
+def agent_PPO(prompt, num_legal_actions, num_possible_obs):
+    dummy_env.set_meta(num_legal_actions, num_possible_obs)
+    meta = (num_legal_actions, num_possible_obs)
+    num_observs = (len(prompt)+1)/3
+    train_on_len = 3*pow(2, int(log2(num_observs)))-1
+    train_on = prompt[:train_on_len]
 
-            A.learn(len(rewards)-1)
-            cache[(train_on, meta)] = A
-        else:
-            A = cache[(train_on, meta)]
+    if not((train_on, meta) in cache_PPO):
+        rewards = [train_on[i+0] for i in range(0,train_on_len,3)]
+        observs = [train_on[i+1] for i in range(0,train_on_len,3)]
+        dummy_env.set_rewards_and_observs(rewards, observs)
+        n_steps = len(rewards)-1
 
-        action, _ = A.predict(prompt[-1])
-        return action
+        if n_steps < 2:
+            return 0
 
-    return agent
+        A = SBL3.PPO('MlpPolicy', dummy_env, n_steps=n_steps, batch_size=n_steps, seed=0)
 
-agent_A2C = SBL_agent(SBL3.A2C)
-agent_DQN = SBL_agent(SBL3.DQN)
-agent_PPO = SBL_agent(SBL3.PPO)
+        A.learn(len(rewards)-1)
+        cache_PPO[(train_on, meta)] = A
+    else:
+        A = cache_PPO[(train_on, meta)]
+
+    action, _ = A.predict(prompt[-1])
+    return action
+
+def agent_DQN(prompt, num_legal_actions, num_possible_obs):
+    dummy_env.set_meta(num_legal_actions, num_possible_obs)
+    meta = (num_legal_actions, num_possible_obs)
+    num_observs = (len(prompt)+1)/3
+    train_on_len = 3*pow(2, int(log2(num_observs)))-1
+    train_on = prompt[:train_on_len]
+
+    if not((train_on, meta) in cache_DQN):
+        rewards = [train_on[i+0] for i in range(0,train_on_len,3)]
+        observs = [train_on[i+1] for i in range(0,train_on_len,3)]
+        dummy_env.set_rewards_and_observs(rewards, observs)
+
+        A = SBL3.DQN('MlpPolicy', dummy_env, train_freq=len(rewards)-1, seed=0)
+
+        A.learn(len(rewards)-1)
+        cache_DQN[(train_on, meta)] = A
+    else:
+        A = cache_DQN[(train_on, meta)]
+
+    action, _ = A.predict(prompt[-1])
+    return action
