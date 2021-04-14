adb9a2ffb351685f56f38e5e9ee6e28fffeb087f
Samuel Alexander <sam@donotemail.com>
2021-04-13 22:24:05 -0400

    Add tempfile dqn.py

---
+++ src/dqn.py
@@ -0,0 +1,7 @@
+from agents.custom_DQN import custom_DQN_agent
+from GuardedTreasures import GuardedTreasures # NOTE: NOT the file from your branch
+from util import run_environment
+
+N = 5000
+result = run_environment(GuardedTreasures, custom_DQN_agent, N)
+print('Total reward/N: ' + str(result['total_reward']/N))
