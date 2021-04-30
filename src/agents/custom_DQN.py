f5f925fed66e1ed473546c5e08233e1ddebc9a2f
Samuel Alexander <sam@donotemail.com>
2021-04-29 23:42:49 -0400

    Remove unused import

--- src/agents/custom_DQN.py
+++ src/agents/custom_DQN.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision.transforms as T
 import numpy as np
 from collections import namedtuple
 import datetime as dt
