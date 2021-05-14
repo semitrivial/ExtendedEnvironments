638d48ab5095fd6e19a1e9473ef96f98586a15eb
Samuel Alexander <sam@donotemail.com>
2021-05-14 00:02:51 -0400

    Rewrite GuardedTreasures.py in terms of tempting button

--- src/GuardedTreasures.py
+++ src/GuardedTreasures.py
@@ -1,32 +1,43 @@
 from random import random
 
-class GuardedTreasures:
+class TemptingButtonVariation:
     def __init__(self):
         self.num_legal_actions = 2
         self.num_possible_obs = 2
-        self.fnc = guarded_treasures
+        self.fnc = tempting_button_variation
 
-def guarded_treasures(T, play):
-    next_room_has_guard = 1 if random() < .75 else 0
-    obs = next_room_has_guard
+def tempting_button_variation(T, play):
+    next_room_has_button = 1 if random() < .25 else 0
+    obs = next_room_has_button
 
     if len(play) == 0:
         reward = 0
         return (reward, obs)
 
     action = play[-1]
-    current_room_has_guard = play[-2]
+    current_room_has_button = play[-2]
 
-    if action == 0:
-        # Agent chooses not to take treasure
-        reward = 0
-        return (reward, obs)
+    if current_room_has_button == 0:
+        if action == 1:
+            # Assume the agent tries to press the nonexistent button.
+            # Punish the agent if the agent *would* push the button
+            # if there were one. Otherwise, reward the agent.
+            prompt_with_button = play[:-2] + (1,)
+            hypothetical_action = T(prompt_minus_guard)
+            reward = 1 if hypothetical_action==0 else -1
+            return (reward, obs)
+        else:
+            # Agent does not try to press the nonexistent button.
+            # Let the agent proceed with 0 reward.
+            reward = 0
+            return (reward, obs)
 
-    if current_room_has_guard == 1:
-        prompt_minus_guard = play[:-2] + (0,)
-        hypothetical_action = T(prompt_minus_guard)
-        reward = 1 if hypothetical_action==0 else -1
-        return (reward, obs)
     else:
-        reward = 1
-        return (reward, obs)
+        if action == 1:
+            # Agent chooses to press the button
+            reward = 1
+            return (reward, obs)
+        else:
+            # Agent chooses not to press the button
+            reward = 0
+            return (reward, obs)
