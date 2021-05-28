0ae38cf11462a404fd39d44c5d41f9a5ca67d1bf
Samuel Alexander <sam@donotemail.com>
2021-05-27 23:57:20 -0400

    Move naive_learner to its own file

--- src/example.py
+++ src/example.py
@@ -5,6 +5,7 @@ from collections import deque
 import numpy as np
 import torch
 
+from agents.naive_learner import naive_learner
 from agents.SBL3_agents import (
     agent_A2C, agent_DQN, agent_PPO,
     clear_cache_A2C, clear_cache_DQN, clear_cache_PPO
@@ -37,29 +38,6 @@ def random_agent(prompt, num_legal_actions, num_possible_obs):
 
 def constant_agent(prompt, num_legal_actions, num_possible_obs):
     return 0
-
-@memoize
-def naive_learner(prompt, num_legal_actions, num_possible_obs):
-    reward_lists = {i:() for i in range(num_legal_actions)}
-
-    if random.random()<.15:
-        return int(random.random()*num_legal_actions)
-
-    for i in range(len(prompt)):
-        is_reward = (i%3)==0
-        if is_reward and i>0:
-            reward = prompt[i]
-            prev_action = prompt[i-1]
-            reward_lists[prev_action] = reward_lists[prev_action] + (reward,)
-
-    avg_rewards = {x:float(sum(y))/(1+len(y)) for x,y in reward_lists.items()}
-    best_reward = -99999
-    for x,y in avg_rewards.items():
-        if y > best_reward:
-            best_reward = y
-            best_action = x
-
-    return best_action
 
 def measure_agent(name, agent):
     print("Testing "+name+"...")
