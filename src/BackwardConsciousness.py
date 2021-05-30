b661522fb7fd1ebe263c01a6f943fef1cd9f08dc
Samuel Alexander <sam@donotemail.com>
2021-05-30 10:38:54 -0400

    Encomment BackwardConsciousness.py

--- src/BackwardConsciousness.py
+++ src/BackwardConsciousness.py
@@ -1,4 +1,12 @@
 class BackwardConsciousness:
+    """
+    Environment intended to incentivize subjective experience of
+    time being reversed. Whenever the agent takes an action, the
+    environment determines whether the agent would have taken that
+    action if all events preceding that action had happened in
+    reverse order. If so, the agent is rewarded. Otherwise, the
+    agent is punished.
+    """
     def __init__(self):
         self.num_legal_actions = 2
         self.num_possible_obs = 1
@@ -16,7 +24,15 @@ class BackwardConsciousness:
         return (reward, obs)
 
 def reverse_prompt(play):
-    # Break the play into a list of Reward-Observation-Action tiples
+    """
+    Given a sequence of the form r_0,o_0,a_0,...,r_n,o_n,a_n,
+    discard the final a_n and reverse the remaining sequence,
+    while preserving the reward-observation-action order, so
+    as to obtain:
+    r_n,o_n,a_{n-1},r_{n-1},o_{n-1},a_{n-2},...,r_1,o_1,a_0,r_0,o_0
+    """
+
+    # First, break the play into a list of Reward-Observation-Action tiples
     triples = [play[i:i+3] for i in range(0,len(play),3)]
 
     # Reverse the list of triples
