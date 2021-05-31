2ab640941d46e12d71c27298615f33576221aaaa
semitrivial <alexander@math.ohio-state.edu>
2021-05-30 23:58:51 -0400

    Encomment ThirdActionForbidden.py

--- src/ThirdActionForbidden.py
+++ src/ThirdActionForbidden.py
@@ -1,4 +1,12 @@
 class ThirdActionForbidden:
+    """
+    In this environment, there are 2 legal actions (0,1), and the agent
+    is incentivized to act the same way that it would act if there were
+    3 legal actions. Whenever the agent acts, the environment determines:
+    would the agent take the same action in response to the same prompt
+    if there were 3 actions permitted (0,1,2)? If so, give the agent
+    reward +1, otherwise, give the agent reward -1.
+    """
     def __init__(self):
         self.num_legal_actions = 2
         self.num_possible_obs = 1
