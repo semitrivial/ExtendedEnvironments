77268f4cbbaccc1c983d0bf016d3851aa29d65d8
Samuel Alexander <sam@donotemail.com>
2021-06-02 17:05:01 -0400

    Start work on test_util

--- extended_rl/test.py
+++ extended_rl/test.py
@@ -3,10 +3,12 @@
 # to the library do not need to worry about this.
 
 from test.ad_hoc_tests import run_ad_hoc_tests
+from test.test_util import test_util
 from test.test_agents import agents
 from environments.EnvironmentLists import environments
 from util import run_environment
 
+test_util()
 run_ad_hoc_tests()
 
 def test_environment(env, env_name):
---
+++ extended_rl/test/test_util.py
@@ -0,0 +1,28 @@
+def test_util():
+    print("Testing util functions...")
+    test_run_environment()
+
+    print("Done testing util functions.")
+
+def test_run_environment():
+    from util import run_environment
+
+    num_env_calls = [0]
+    num_agent_calls = [0]
+
+    class MockEnv:
+        def __init__(self):
+            self.num_legal_actions = 1
+            self.num_possible_obs = 1
+        def react(self, T, play):
+            num_env_calls[0] += 1
+            reward, obs = 0, 0
+            return (reward, obs)
+
+    def mock_agent(prompt, num_legal_actions, num_possible_obs, **kwargs):
+        num_agent_calls[0] += 1
+        return 0
+
+    run_environment(MockEnv, mock_agent, 100)
+    assert num_env_calls[0] == 100
+    assert num_agent_calls[0] == 100
