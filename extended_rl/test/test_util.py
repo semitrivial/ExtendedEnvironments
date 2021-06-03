692707696b515cea92d7f99bbf90d957428f9548
Samuel Alexander <sam@donotemail.com>
2021-06-02 21:38:55 -0400

    Expand test_run_environment

--- extended_rl/test/test_util.py
+++ extended_rl/test/test_util.py
@@ -26,3 +26,19 @@ def test_run_environment():
     run_environment(MockEnv, mock_agent, 100)
     assert num_env_calls[0] == 100
     assert num_agent_calls[0] == 100
+
+    num_env_calls[0] = 0
+    num_agent_calls[0] = 0
+
+    class MockEnv2:
+        def __init__(self):
+            self.num_legal_actions = 1
+            self.num_possible_obs = 1
+        def react(self, T, play):
+            num_env_calls[0] += 1
+            T((0,0))
+            return (0,0)
+
+    run_environment(MockEnv2, mock_agent, 100)
+    assert num_env_calls[0] == 100
+    assert num_agent_calls[0] == 200
