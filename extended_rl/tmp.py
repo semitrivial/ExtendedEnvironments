f124e30fa5d4ae0025f86d7ed3d4a1f75938a9b0
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-16 14:52:01 -0400

    Modify tmp.py

--- extended_rl/tmp.py
+++ extended_rl/tmp.py
@@ -7,9 +7,13 @@ from environments.AfterImages import AfterImages
 from agents.reality_check import reality_check
 
 A = Q_learner(epsilon=0.9, alpha=0.1, gamma=0.9)
-#A = reality_check(A)
 env = AfterImages
 n_steps = 100000
 
+print("Without reality_check:")
 results = fast_run_env(env, A, n_steps)
+print("Avg Reward: "+str(results['total_reward']/n_steps))
+
+print("With reality check:")
+results = fast_run_env(env, reality_check(A), n_steps)
 print("Avg Reward: "+str(results['total_reward']/n_steps))
