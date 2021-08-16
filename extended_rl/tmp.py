04af1120e710c65f7c1213771f3cacc5b0c596a0
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-16 11:42:58 -0400

    Reduce a number

--- extended_rl/tmp.py
+++ extended_rl/tmp.py
@@ -4,7 +4,7 @@ from environments.IgnoreRewards import IgnoreRewards
 
 A = Q_learner(epsilon=0.9, alpha=0.1, gamma=0.9)
 env = IgnoreRewards
-n_steps = 1000000
+n_steps = 100000
 
 results = fast_run_env(env, A, n_steps)
 print("Avg Reward: "+str(results['total_reward']/n_steps))
