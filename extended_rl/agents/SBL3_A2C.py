277e0fcdc2382a8c8914f47d8ccb2057df1c7915
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-21 16:01:43 -0400

    Add SBL3_A2C.py

---
+++ extended_rl/agents/SBL3_A2C.py
@@ -0,0 +1,40 @@
+from stable_baselines3 import A2C as A2C_factory
+
+from agents.SBL3_util import DummyGymEnv, create_fwd_monkeypatch
+
+
+NSTEPS=5  # SBL3's default n_steps for A2C
+
+class A2C_learner:
+    def __init__(self, env, **kwargs):
+        self.gym = DummyGymEnv()
+        self.gym.set_meta(env.num_legal_actions, env.num_possible_obs)
+        self.worker = A2C_factory('MlpPolicy', self.gym, **kwargs)
+        self.worker_forward = self.worker.policy.forward
+        self.actions = []
+        self.history = []
+        self.monkeypatch = create_fwd_monkeypatch(self, NSTEPS)
+        self.fInitialObs = False
+        self.training_cnt = 0
+
+    def act(self, obs):
+        action, _ = self.worker.predict(obs)
+        return action
+
+    def train(self, o_prev, act, R, o_next):
+        if not self.fInitialObs:
+            self.gym.set_initial_obs(o_prev)
+            self.fInitialObs = True
+
+        self.history += [act, R, o_next]
+        self.actions += [act]
+        self.training_cnt += 1
+
+        if self.training_cnt == NSTEPS:
+            self.training_cnt = 0
+            self.gym.set_history(self.history)
+            self.worker.policy.forward = self.monkeypatch
+            self.worker.learn(NSTEPS)
+            self.worker.policy.forward = self.worker_forward
+            self.history = []
+            self.actions = []            
