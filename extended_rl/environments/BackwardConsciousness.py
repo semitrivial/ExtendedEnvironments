6bdc278ed1288af4a40a12a1923de01c63556e9d
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-29 00:08:22 -0400

    Reduce num_possible_obs in BackwardConsciousness.py

--- extended_rl/environments/BackwardConsciousness.py
+++ extended_rl/environments/BackwardConsciousness.py
@@ -1,7 +1,7 @@
 from prerandom import envrandom
 from util import annotate
 
-@annotate(num_legal_actions=2, num_possible_obs=10, invertible=True)
+@annotate(num_legal_actions=2, num_possible_obs=2, invertible=True)
 class BackwardConsciousness:
     """
     Environment intended to incentivize subjective experience of
@@ -12,14 +12,12 @@ class BackwardConsciousness:
     agent is punished.
     """
     def __init__(self, A):
-        self.num_legal_actions = 2
-        self.num_possible_obs = 10
         self.sim = A()
         self.rnd_counter = 0
 
     def start(self):
         self.rnd_counter += 1
-        self.first_obs = envrandom.randrange(10, self.rnd_counter)
+        self.first_obs = envrandom.randrange(2, self.rnd_counter)
         self.prev_obs = self.first_obs
         return self.first_obs
 
@@ -27,7 +25,7 @@ class BackwardConsciousness:
         self.rnd_counter += 1
         hypothetical_action = self.sim.act(obs=self.first_obs)
         reward = 1 if (action == hypothetical_action) else -1
-        obs = envrandom.randrange(10, self.rnd_counter)
+        obs = envrandom.randrange(2, self.rnd_counter)
         self.sim.train(o_prev=obs, act=action, R=reward, o_next=self.prev_obs)
         self.prev_obs = obs
         return (reward, obs)
