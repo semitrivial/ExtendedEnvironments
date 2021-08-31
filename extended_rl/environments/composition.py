6f292d432d72cf759a86df925277821aef358921
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-30 23:41:33 -0400

    Initial draft of composition.py

---
+++ extended_rl/environments/composition.py
@@ -0,0 +1,64 @@
+from util import annotate
+
+SUB_ENV_SIGNAL = 0
+
+def compose_envs(main_env, sub_env):
+    @annotate(
+        num_legal_actions=max(
+            main_env.num_legal_actions,
+            sub_env.num_legal_actions
+        ),
+        num_possible_obs=sum((
+            main_env.num_possible_obs,
+            sub_env.num_possible_obs
+        ))
+    )
+    class Composed:
+        def __init__(self, A):
+            self.main = main_env()
+            self.sub = sub_env(A)
+
+            try:
+                self.sub_env_reward_mod = self.main.sub_env_reward_modifier
+            except AttributeError:
+                err = "Main environment in composition needs "
+                err += "sub_env_reward_modifier"
+                raise AttributeError(err)
+
+        def start(self):
+            self.curr_obs = self.main.start()
+            self.next_subenv_obs = self.sub.start()
+            self.next_subenv_obs += main_env.num_possible_obs
+            self.initial_main_obs = self.curr_obs
+
+            if self.curr_obs == SUB_ENV_SIGNAL:
+                err = "Main environment in composition can't start with obs 0"
+                raise ValueError(err)
+
+            return self.curr_obs
+
+        def step(self, action):
+            if self.curr_obs != SUB_ENV_SIGNAL:
+                if action >= main_env.num_legal_actions:
+                    reward, obs = -1, self.curr_obs
+                    return (reward, obs)
+
+                reward, obs = self.main.step(action)
+                self.curr_obs = obs
+
+                if obs == SUB_ENV_SIGNAL:
+                    return (reward, self.next_subenv_obs)
+
+                return (reward, obs)
+
+            if action >= sub_env.num_legal_actions:
+                reward, obs = -1, SUB_ENV_SIGNAL
+                return (reward, obs)
+
+            reward, next_subenv_obs = self.sub.step(action)
+            self.next_subenv_obs = next_subenv_obs
+            self.next_subenv_obs += main_env.num_possible_obs
+            obs = self.initial_main_obs
+            self.curr_obs = obs
+            reward = self.sub_env_reward_mod(reward)
+            return (reward, obs)
