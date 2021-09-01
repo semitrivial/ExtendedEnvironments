008c8efc03b9dd5a3e46b36be0233f947fdbf261
Samuel Alexander <alexander@math.ohio-state.edu>
2021-08-31 22:08:12 -0400

    Change SimplePath to SimpleTrap

--- extended_rl/environments/Vanilla.py
+++ extended_rl/environments/Vanilla.py
@@ -8,8 +8,8 @@ def mark_as_vanilla_env(env_class):
     return env_class
 
 @mark_as_vanilla_env
-@annotate(num_legal_actions=3, num_possible_obs=3)
-class SimplePath:
+@annotate(num_legal_actions=10, num_possible_obs=10)
+class SimpleTrap:
     def __init__(self):
         pass
     def start(self):
@@ -31,9 +31,12 @@ class SimplePath:
                 reward = 0
                 return (reward, self.curr_obs)
         else:
+            if self.curr_obs == self.num_possible_obs-3 and action==0:
+                self.curr_obs = 1
+                reward = 1  # Trap to catch greedy agents
+                return (reward, self.curr_obs)
             reward = -1
             return (reward, self.curr_obs)
 
-    @staticmethod
-    def sub_env_reward_modifier(reward):
-        return reward*4 if reward>0 else reward
+    def sub_env_reward_modifier(self, reward):
+        return reward*(self.num_possible_obs+1) if reward>0 else 0
