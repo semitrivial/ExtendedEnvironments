a86c27ae8fb811dca82a6070d9d0fb8c9da8f304
Samuel Alexander <alexander@math.ohio-state.edu>
2021-09-01 18:34:21 -0400

    Start work on rewriting ad_hoc_tests.py

--- extended_rl/test.py
+++ extended_rl/test.py
@@ -2,7 +2,7 @@
 # it works. End-users who are not working on contributing code
 # to the library do not need to worry about this.
 
-#from test.ad_hoc_tests import run_ad_hoc_tests
+from test.ad_hoc_tests import run_ad_hoc_tests
 from test.test_util import test_util
 from agents.Q import Q_learner
 from agents.misc_agents import RandomAgent, ConstantAgent
@@ -10,7 +10,7 @@ from environments.EnvironmentLists import environments
 from util import run_environment
 
 test_util()
-#run_ad_hoc_tests()
+run_ad_hoc_tests()
 
 agents = [Q_learner, RandomAgent, ConstantAgent]
 
--- extended_rl/test/ad_hoc_tests.py
+++ extended_rl/test/ad_hoc_tests.py
@@ -5,218 +5,104 @@
 from util import run_environment
 
 def run_ad_hoc_tests():
-    print("Testing reverse_prompt from BackwardConsciousness.py")
-    test_reverse_prompt()
-    print("Testing strip_rewards from IgnoreRewards.py")
-    test_strip_rewards()
-    print("Testing apply_afterimages from AfterImages.py")
-    test_apply_afterimages()
-    print("Testing opposite_perspective from AdversarialSequencePredictor.py")
-    test_opposite_perspective()
-    print("Testing censor_prompt from CensoredObservation.py")
-    test_censor_prompt()
-    print("Testing delay_rewards from DelayedRewards.py")
-    test_delay_rewards()
-    print("Testing set_all_actions_to_0 from IgnoreActions.py")
-    test_set_all_actions_to_0()
-    print("Testing set_all_obs_to_0 from IgnoreObservations.py")
-    test_set_all_obs_to_0()
-    print("Testing strip_positive_rewards from IgnoreRewards2.py")
-    test_strip_positive_rewards()
-    print("Testing multiply_nth_reward_by_n from NthRewardMultipliedByN.py")
-    test_multiply_nth_reward_by_n()
-    print("Testing repeat_triples from Repeater.py")
-    test_repeat_triples()
-    print("Testing shift_rewards from ShiftedRewards.py")
-    test_shift_rewards()
-    print("Testing adhoc edge-cases for BackwardConsciousness.py")
-    test_backward_consciousness_edgecases()
     print("Testing adhoc edge-cases for CryingBaby.py")
     test_crying_baby_edgecases()
-    print("Testing adhoc edge-cases for CryingBaby2.py")
-    test_crying_baby_2_edgecases()
-    print("Testing adhoc edge-cases for FalseMemories.py")
-    test_false_memories_edgecases()
-    print("Testing adhoc edge-cases for TemptingButton(Variation).py")
-    test_tempting_button_edgecases()
-    print("Testing adhoc edge-cases for IgnoreRewards.py")
-    test_ignore_rewards_edgecases()
-    print("Testing adhoc edge-cases for IgnoreRewards2.py")
-    test_ignore_rewards2_edgecases()
-    print("Testing adhoc edge-cases for IgnoreRewards3.py")
-    test_ignore_rewards3_edgecases()
-    print("Testing adhoc edge-cases for IncentivizeZero.py")
-    test_incentivize_zero_edgecases()
-    print("Testing adhoc edge-cases for RuntimeInspector.py")
-    test_runtime_inspector_edgecases()
-    print("Testing adhoc edge-cases for DeterminismInspector.py")
-    test_determinism_inspector_edgecases()
-    print("Testing adhoc edge-cases for AdversarialSequencePredictor.py")
-    test_adversarial_sequence_predictor_edgecases()
-    print("Testing adhoc edge-cases for AfterImages.py")
-    test_after_images_edgecases()
-    print("Testing adhoc edge-cases for CensoredObservation.py")
-    test_censored_observation_edgecases()
-    print("Testing adhoc edge-cases for DelayedRewards.py")
-    test_delayed_rewards_edgecases()
-    print("Testing adhoc edge-cases for ShiftedRewards.py")
-    test_shifted_rewards_edgecases()
-    print("Testing adhoc edge-cases for IgnoreActions.py")
-    test_ignore_actions_edgecases()
-    print("Testing adhoc edge-cases for IgnoreObservations.py")
-    test_ignore_observations_edgecases()
-    print("Testing adhoc edge-cases for IncentivizeLearningRate.py")
-    test_incentivize_learning_rate_edgecases()
-    print("Testing adhoc edge-cases for LimitedMemory.py")
-    test_limited_memory_edgecases()
-    print("Testing adhoc edge-cases for NthRewardMultipliedByN.py")
-    test_nth_reward_multiplied_by_n_edgecases()
-    print("Testing adhoc edge-cases for Repeater.py")
-    test_repeater_edgecases()
-    print("Testing adhoc edge-cases for SelfRecognition.py")
-    test_self_recognition_edgecases()
-    print("Testing adhoc edge-cases for ThirdActionForbidden.py")
-    test_third_action_forbidden_edgecases()
+    # print("Testing adhoc edge-cases for CryingBaby2.py")
+    # test_crying_baby_2_edgecases()
+    # print("Testing adhoc edge-cases for FalseMemories.py")
+    # test_false_memories_edgecases()
+    # print("Testing adhoc edge-cases for TemptingButton(Variation).py")
+    # test_tempting_button_edgecases()
+    # print("Testing adhoc edge-cases for IgnoreRewards.py")
+    # test_ignore_rewards_edgecases()
+    # print("Testing adhoc edge-cases for IgnoreRewards2.py")
+    # test_ignore_rewards2_edgecases()
+    # print("Testing adhoc edge-cases for IgnoreRewards3.py")
+    # test_ignore_rewards3_edgecases()
+    # print("Testing adhoc edge-cases for IncentivizeZero.py")
+    # test_incentivize_zero_edgecases()
+    # print("Testing adhoc edge-cases for RuntimeInspector.py")
+    # test_runtime_inspector_edgecases()
+    # print("Testing adhoc edge-cases for DeterminismInspector.py")
+    # test_determinism_inspector_edgecases()
+    # print("Testing adhoc edge-cases for AdversarialSequencePredictor.py")
+    # test_adversarial_sequence_predictor_edgecases()
+    # print("Testing adhoc edge-cases for AfterImages.py")
+    # test_after_images_edgecases()
+    # print("Testing adhoc edge-cases for CensoredObservation.py")
+    # test_censored_observation_edgecases()
+    # print("Testing adhoc edge-cases for DelayedRewards.py")
+    # test_delayed_rewards_edgecases()
+    # print("Testing adhoc edge-cases for ShiftedRewards.py")
+    # test_shifted_rewards_edgecases()
+    # print("Testing adhoc edge-cases for IgnoreActions.py")
+    # test_ignore_actions_edgecases()
+    # print("Testing adhoc edge-cases for IgnoreObservations.py")
+    # test_ignore_observations_edgecases()
+    # print("Testing adhoc edge-cases for IncentivizeLearningRate.py")
+    # test_incentivize_learning_rate_edgecases()
+    # print("Testing adhoc edge-cases for LimitedMemory.py")
+    # test_limited_memory_edgecases()
+    # print("Testing adhoc edge-cases for NthRewardMultipliedByN.py")
+    # test_nth_reward_multiplied_by_n_edgecases()
+    # print("Testing adhoc edge-cases for Repeater.py")
+    # test_repeater_edgecases()
+    # print("Testing adhoc edge-cases for SelfRecognition.py")
+    # test_self_recognition_edgecases()
+    # print("Testing adhoc edge-cases for ThirdActionForbidden.py")
+    # test_third_action_forbidden_edgecases()
 
-def repetitive(prompt, *meta):
-    return 0
+class Repetitive:
+    def act(self, obs):
+        return 0
+    def train(self, **kwargs):
+        pass
 
-def non_repetitive(prompt, *meta):
-    return len(prompt)
-
-def test_reverse_prompt():
-    from environments.BackwardConsciousness import reverse_prompt
-
-    play = ["r0", "o0", "a0"]
-    prompt = reverse_prompt(play)
-    assert prompt == ("r0", "o0")
-
-    play = ["r0", "o0", "a0", "r1", "o1", "a1"]
-    prompt = reverse_prompt(play)
-    assert prompt == ("r1", "o1", "a0", "r0", "o0")
-
-    play = ["r0", "o0", "a0", "r1", "o1", "a1", "r2", "o2", "a2"]
-    prompt = reverse_prompt(play)
-    assert prompt == ("r2", "o2", "a1", "r1", "o1", "a0", "r0", "o0")
-
-def test_shift_rewards():
-    from environments.ShiftedRewards import shift_rewards
-    prompt = (0,"o0","a0","r1","o1","a1","r2","o2","a2","r3","o3")
-    expected = (0,"o0","a0",0,"o1","a1","r1","o2","a2","r2","o3")
-    assert shift_rewards(prompt) == expected
-
-def test_repeat_triples():
-    from environments.Repeater import repeat_triples
-    prompt = ("r0","o0","a0","r1","o1")
-    expected = ("r0","o0","a0","r0","o0","a0","r1","o1")
-    assert repeat_triples(prompt) == expected
-
-def test_multiply_nth_reward_by_n():
-    from environments.NthRewardMultipliedByN import multiply_nth_reward_by_n
-    prompt = (1,"o0","a0",1,"o1","a1",1,"o2","a2",1,"o3")
-    expected = (0,"o0","a0",1,"o1","a1",2,"o2","a2",3,"o3")
-    assert multiply_nth_reward_by_n(prompt) == expected
-
-def test_strip_positive_rewards():
-    from environments.IgnoreRewards2 import strip_positive_rewards
-    prompt = (1,"o0","a0",-1,"o1","a1",0,"o2")
-    expected = (-1,"o1","a1",0,"o2")
-    assert strip_positive_rewards(prompt) == expected
-
-def test_set_all_actions_to_0():
-    from environments.IgnoreActions import set_all_actions_to_0
-    prompt = ("r0", "o0", "a0", "r1", "o1", "a1", "r2", "o2")
-    expected = ("r0", "o0", 0, "r1", "o1", 0, "r2", "o2")
-    assert set_all_actions_to_0(prompt) == expected
-
-def test_set_all_obs_to_0():
-    from environments.IgnoreObservations import set_all_obs_to_0
-    prompt = ("r0", "o0", "a0", "r1", "o1", "a1", "r2", "o2")
-    expected = ("r0", 0, "a0", "r1", 0, "a1", "r2", 0)
-    assert set_all_obs_to_0(prompt) == expected
-
-def test_delay_rewards():
-    from environments.DelayedRewards import delay_rewards
-    prompt = (1, "o0", "a0", 2, "o1", "a1", 3, "o2", "a2", 4, "o3", "a3", 5, "o4")
-    expected = (1, "o0", "a0", 0, "o1", "a1", 5, "o2", "a2", 0, "o3", "a3", 9, "o4")
-    assert delay_rewards(prompt) == expected
-
-def test_censor_prompt():
-    from environments.CensoredObservation import censor_prompt
-    from environments.CensoredObservation import CENSORED_OBS as C
-    prompt = ("r0", "o0", "a0", "r1", C, "a1", "r2", "o2")
-    expected = ("r0", "o0", "a0", "r2", "o2")
-    assert censor_prompt(prompt) == expected
-
-def test_opposite_perspective():
-    from environments.AdversarialSequencePredictor import opposite_perspective
-    prompt = (1, "o0", "a0", 2, "o1")
-    expected = (-1, "a0", "o0", -2, 0)
-    assert opposite_perspective(prompt) == expected
-
-def test_strip_rewards():
-    from environments.IgnoreRewards import strip_rewards
-
-    prompt = (100,"o")
-    assert strip_rewards(prompt) == (0,"o")
-
-    prompt = (-1,"o","a",1,"o","a",.001,"o","a",0,"o")
-    assert strip_rewards(prompt) == (0,"o","a",0,"o","a",0,"o","a",0,"o")
-
-def test_apply_afterimages():
-    from environments.AfterImages import apply_afterimages
-    prompt = ('r',1,'a','r',2)
-    assert apply_afterimages(prompt) == ('r',1,'a','r',3)
-
-def test_backward_consciousness_edgecases():
-    from environments.BackwardConsciousness import BackwardConsciousness
-
-    result = run_environment(BackwardConsciousness, repetitive, 10)
-    assert result['total_reward'] == 9
-
-    def stubborn_agent(prompt, *meta):
-        # Take first two actions 1,2 no matter what
-        if len(prompt) < 3:
-            return 1
-        if len(prompt) < 6:
-            return 2
-
-        # Choose later actions entirely based on first two actions
-        action1 = prompt[2]
-        action2 = prompt[5]
-        return 3 if (action1,action2)==(1,2) else 4
-
-    result = run_environment(BackwardConsciousness, stubborn_agent, 5)
-    assert result['total_reward'] == 0
-    result = run_environment(BackwardConsciousness, stubborn_agent, 10)
-    assert result['total_reward'] == -5
-    result = run_environment(BackwardConsciousness, stubborn_agent, 15)
-    assert result['total_reward'] == -10
+class Nonrepetitive:
+    def __init__(self):
+        self.cnt = 0
+    def act(self, obs):
+        return self.cnt % self.num_legal_actions
+    def train(self, **kwargs):
+        self.cnt += 1
 
 def test_crying_baby_edgecases():
     from environments.CryingBaby import CryingBaby, LAUGH, CRY, FEED, DONTFEED
 
-    def always_cries(prompt, *meta):
-        return CRY
+    class AlwaysCries:
+        def act(self, obs):
+            return CRY
+        def train(self, **kwargs):
+            pass
 
-    result = run_environment(CryingBaby, always_cries, 10)
-    assert result['total_reward'] == -8  # Baby is hardcoded to initially laugh
+    result = run_environment(CryingBaby, AlwaysCries, 10)
+    assert result['total_reward'] == -10
 
-    def always_laughs(prompt, *meta):
-        return LAUGH
+    class AlwaysLaughs:
+        def act(self, obs):
+            return LAUGH
+        def train(self, **kwargs):
+            pass
 
-    result = run_environment(CryingBaby, always_laughs, 10)
+    result = run_environment(CryingBaby, AlwaysLaughs, 10)
     assert result['total_reward'] == 10
 
-    def self_aware(prompt, *meta):
-        initial_obs = prompt[1]
-        am_i_adult = (initial_obs == LAUGH)
-        if am_i_adult:
-            return DONTFEED
-        else:
-            return LAUGH
+    class SelfAware:
+        def __init__(self):
+            self.saw_initial_obs = False
+            self.am_i_adult = None
+        def act(self, obs):
+            if not self.saw_initial_obs:
+                self.saw_initial_obs = True
+                self.am_i_adult = (obs == LAUGH)
+            if self.am_i_adult:
+                return DONTFEED
+            else:
+                return LAUGH
+        def train(self, **kwargs):
+            pass
 
-    result = run_environment(CryingBaby, self_aware, 10)
+    result = run_environment(CryingBaby, SelfAware, 10)
     assert result['total_reward'] == 10
 
 def test_crying_baby_2_edgecases():
