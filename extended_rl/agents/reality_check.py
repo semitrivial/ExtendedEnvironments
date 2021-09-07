167318694d630888f8a70e19f3ab3baf38e1c62e
Samuel Alexander <alexander@math.ohio-state.edu>
2021-09-07 13:14:23 -0400

    Fix a bug in reality_check.py

--- extended_rl/agents/reality_check.py
+++ extended_rl/agents/reality_check.py
@@ -9,12 +9,14 @@ def reality_check(A0):
       self.underlying = A0_with_meta(**kwargs)
       self.first_action = None
       self.expected_training_action = None
+      self.expected_training_oprev = None
       self.found_unexpected_action = False
 
     def act(self, obs):
       if self.found_unexpected_action:
         return self.first_action
 
+      self.expected_training_oprev = obs
       if self.first_action is None:
         self.first_action = self.underlying.act(obs)
         self.expected_training_action = self.first_action
@@ -26,6 +28,8 @@ def reality_check(A0):
     def train(self, o_prev, act, R, o_next):
       if not self.found_unexpected_action:
         if act == self.expected_training_action:
+          self.underlying.train(o_prev, act, R, o_next)
+        elif o_prev != self.expected_training_oprev:
           self.underlying.train(o_prev, act, R, o_next)
         else:
           if self.first_action is None:
