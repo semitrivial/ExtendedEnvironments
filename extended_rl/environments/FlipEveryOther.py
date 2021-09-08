108c2abffec065f2952bc95ce22ee89bb60f19d0
Samuel Alexander <alexander@math.ohio-state.edu>
2021-09-08 16:42:04 -0400

    Add comment to FlipEveryOther.py

--- extended_rl/environments/FlipEveryOther.py
+++ extended_rl/environments/FlipEveryOther.py
@@ -2,6 +2,15 @@ from util import annotate
 
 @annotate(num_legal_actions=2, num_possible_obs=1)
 class FlipEveryOther:
+    """
+    Environment in which the agent is rewarded for acting as if every other
+    reward had its sign flipped. Whenever the agent acts, the environment
+    determines: would the agent have acted the same way if, hypothetically,
+    everything that has happened so far had happened except that the 1st,
+    3rd, 5th, 7th, ... rewards had had their sign flipped? If so, the
+    environment gives the agent +1 reward, otherwise the environment gives
+    the agent -1 reward.
+    """
     def __init__(self, A):
         self.sim = A()
         self.mult = 1
