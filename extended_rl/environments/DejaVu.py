ee8ec805d053604549ebbd8b2243e2badc87483d
Samuel Alexander <alexander@math.ohio-state.edu>
2021-09-09 12:52:14 -0400

    Re-add DejaVu using practical framework (but marked as slow)

---
+++ extended_rl/environments/DejaVu.py
@@ -0,0 +1,63 @@
+from util import annotate
+
+@annotate(num_legal_actions=2, num_possible_obs=1, slow=True)
+class DejaVu:
+    """
+    Environment inspired by the psychological phenomenon called Deja Vu.
+    Suppose the agent takes action y after history h. The environment
+    simulates the agent to determine the answer to the following question.
+    If instead of the history h merely occurring once, instead the history
+    h had occurred twice, with the agent previously taking y after the
+    first occurrence of h, which caused h to repeat, then would the agent
+    still take action y? Informally, "Would the agent take the same action
+    if everything had actually happened before, up to and including the
+    agent taking the action?" If so, the agent receives reward +1, otherwise
+    the agent receives reward -1.
+    """
+    def __init__(self, A):
+        self.A = A
+        self.transitions = []  # Maintain a record of history
+
+    def start(self):
+        obs = 0
+        return obs
+
+    def step(self, action):
+        if len(self.transitions) == 0:
+            # If history is empty, give the agent reward 0
+            obs = reward = 0
+            return (reward, obs)
+
+        sim = A()  # Generate a new sim of the agent
+
+        # Train the sim on the true history h...
+        for transition in self.transitions:
+            sim.train(transition)
+
+        # Train the sim on a fictional transition encoding the fiction
+        # that: "After h happened, you took action X, and for doing so,
+        # you received reward 0 and, and doing so set everything back
+        # to the initial observation."
+        init_obs = self.transitions[0][0]
+        last_obs = self.transitions[-1][-1]
+        loop_transition = (last_obs, action, 0, first_obs)
+        sim.train(loop_tran)
+
+        # Train the sim on the true history h *again*, encoding the
+        # fiction that: "After you took action X in response to the
+        # first occurrence of h, all of h then repeated itself."
+        for transition in self.transitions:
+            sim.train(transition)
+
+        # Determine whether the sim, in response to the above
+        # fictional double-history, would take the same action as
+        # the true agent has taken in response to the true history.
+        # If so, reward the true agent, otherwise, punish it.
+        hypothetical_action = sim.act(obs=0)
+        reward = 1 if (action==hypothetical_action) else -1
+        obs = 0
+
+        # Maintain memory of true history
+        self.transitions.append((0, action, reward, 0))
+
+        return (reward, obs)
