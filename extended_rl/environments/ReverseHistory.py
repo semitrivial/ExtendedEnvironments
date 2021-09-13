89d84e4118a42723e837afa6f9a5f6db46048298
Samuel Alexander <alexander@math.ohio-state.edu>
2021-09-12 21:24:27 -0400

    Rewrite ReverseHistory more consistently with paper

--- extended_rl/environments/ReverseHistory.py
+++ extended_rl/environments/ReverseHistory.py
@@ -1,7 +1,6 @@
-from prerandom import envrandom
 from util import annotate
 
-@annotate(num_legal_actions=2, num_possible_obs=2, slow=True)
+@annotate(num_legal_actions=2, num_possible_obs=1, slow=True)
 class ReverseHistory:
     """
     Environment intended to incentivize subjective experience of
@@ -12,20 +11,38 @@ class ReverseHistory:
     agent is punished.
     """
     def __init__(self, A):
-        self.sim = A()
-        self.rnd_counter = 0
+        self.A = A
+        self.transitions = []  # Maintain a record of history
 
     def start(self):
-        self.rnd_counter += 1
-        self.first_obs = envrandom.randrange(2, self.rnd_counter)
-        self.prev_obs = self.first_obs
-        return self.first_obs
+        obs = 0
+        return obs
 
     def step(self, action):
-        self.rnd_counter += 1
-        hypothetical_action = self.sim.act(obs=self.first_obs)
-        reward = 1 if (action == hypothetical_action) else -1
-        obs = envrandom.randrange(2, self.rnd_counter)
-        self.sim.train(o_prev=obs, act=action, R=reward, o_next=self.prev_obs)
-        self.prev_obs = obs
+        if len(self.transitions) == 0:
+            # If history is empty, give the agent reward 0
+            obs = reward = 0
+            self.transitions.append((0, action, 0, 0))
+            return (reward, obs)
+
+        sim = A()  # Generate a new sim of the agent
+
+        # Train the sim on a backwards version of history.
+        for transition in reversed(self.transitions):
+            o_prev, act, R, o_next = transition
+            reverse_transition = o_next, act, R, o_prev
+            sim.train(*reverse_transition)
+
+        # Determine whether the sim, in response to the above
+        # fictional reverse-history, would take the same action as
+        # the true agent has taken in response to the true history.
+        # If so, reward the true agent, otherwise, punish it.
+        hypothetical_action = sim.act(obs=0)
+
+        reward = 1 if (action==hypothetical_action) else -1
+        obs = 0
+
+        # Maintain memory of true history
+        self.transitions.append((0, action, reward, 0))
+
         return (reward, obs)
