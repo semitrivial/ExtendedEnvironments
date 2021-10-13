1183ff9a03468452db929244c8c7c3e97e392223
Samuel Alexander <alexander@math.ohio-state.edu>
2021-10-13 13:23:02 -0400

    Add 'good classic agent?' column to table

--- extended_rl/experiments/stats.py
+++ extended_rl/experiments/stats.py
@@ -82,10 +82,10 @@ print(r"""
   \caption{Measuring self-reflection of some agents and their reality-checks}
   \label{measurementtable}
   \centering
-  \begin{tabular}{lll}
+  \begin{tabular}{llll}
     \toprule
-    Agent & Measure $\pm$ StdErr & Measure $\pm$ StdErr\\
-          & (Original Agent)     & (Reality Check)\\
+    Agent & Good Classic & Measure $\pm$ StdErr & Measure $\pm$ StdErr\\
+          & Agent? & (Original Agent)     & (Reality Check)\\
     \midrule
 """.strip())
 
@@ -113,7 +113,7 @@ for agent in agents:
             results[orig]['rc'] = (avg, stderr)
 
 template = r"""
-    {agent} & ${orig_measure} \pm {orig_stderr}$ & ${rc_measure} \pm {rc_stderr}$\\
+    {agent} & {goodclassic} & ${orig_measure} \pm {orig_stderr}$ & ${rc_measure} \pm {rc_stderr}$\\
 """[1:-1]
 
 agents_in_order = [
@@ -136,12 +136,23 @@ shortname_dict = {
     "PPO_learner": "PPO"
 }
 
+is_good_classic = {
+    "RandomAgent": "",
+    "ConstantAgent": "",
+    "SimpleLearner": "",
+    "Q_learner": r"$\checkmark$",
+    "DQN_learner": r"$\checkmark$",
+    "A2C_learner": r"$\checkmark$",
+    "PPO_learner": r"$\checkmark$",
+}
+
 for agent in agents_in_order:
     result = results[agent]
     orig_measure, orig_stderr = result['original']
     rc_measure, rc_stderr = result['rc']
     print(template.format(
         agent=shortname_dict[agent],
+        goodclassic=is_good_classic[agent],
         orig_measure=orig_measure,
         orig_stderr=orig_stderr,
         rc_measure=rc_measure,
