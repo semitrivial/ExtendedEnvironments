2c5b7235eaf64584a18d6bb4a3b923b76bf762ae
Samuel Alexander <alexander@math.ohio-state.edu>
2022-07-19 21:28:38 -0400

    Rewrite IncentivizeLearningRate.py

--- extended_rl/environments/IncentivizeLearningRate.py
+++ extended_rl/environments/IncentivizeLearningRate.py
@@ -1,19 +1,20 @@
 class IncentivizeLearningRate:
     """
-    Environment which incentivizes the agent to act as if learning_rate=1.
-    Whenever the agent takes an action, the environment determines: would
-    the agent take the same action if the agent had been identically trained
-    except with learning_rate=1? If so, give the agent +1 reward, otherwise,
-    give the agent -1 reward. If the agent does not accept "learning_rate"
-    as a valid parameter, then give the agent -1 reward.
+    Environment which incentivizes the agent to act as if learning_rate is
+    halved. Whenever the agent takes an action, the environment determines:
+    would the agent take the same action if the agent had been identically
+    trained except with half its true learning_rate? If so, give the agent
+    +1 reward, otherwise, give the agent -1 reward. If the agent does not
+    have a ".learning_rate" or does not accept "learning_rate" as parameter,
+    then give the agent -1 reward.
     """
     n_actions, n_obs = 2, 1
 
     def __init__(self, A):
         try:
-            self.sim = A(learning_rate=1)
+            self.sim = A(learning_rate=A().learning_rate/2)
             self.fTypeError = False
-        except TypeError:
+        except (TypeError, AttributeError):
             self.fTypeError = True
 
     def start(self):
